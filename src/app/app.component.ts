0128ca5d80cd2e36137898082552c205d3ab6bb3
fajarbass <sebastian.fajr@gmail.com>
2022-02-05 00:18:43 +0700

    on student table a,b,c,d,e,f

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,10 @@ import { ApiService } from './api.service';
 import { Student, School, Corrector, Student_Table} from './interface';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator} from '@angular/material/paginator';
+import {FormControl} from '@angular/forms';
+import { Observable, Subscription } from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-root',
@@ -14,18 +18,35 @@ import { MatPaginator} from '@angular/material/paginator';
 export class AppComponent implements OnInit, AfterViewInit {
   title = 'frontendtest';
 
-  studentTbl?: Student_Table[];
+
+  stdControl = new FormControl();
+  schControl = new FormControl();
+  corrControl = new FormControl();
+  ccControl = new FormControl();
+  scControl = new FormControl();
+
+  stdOpt?: Student_Table[];
+  schOpt: any[] = [];
+  stdFilOpt?: Observable<any>;
+  schFilOpt?: Observable<any>;
+  corrFilOpt?: Observable<any>;
+
+  studentTbl: Student_Table[] = [];
   schoolTbl?:School[];
   totalSchool:number = 0;
-  schoolLs?: School[];
+  schoolLs: any[] = [];
   correctorLs:any[]=[];
   schCorrLs: any[] = [];
 
-
   displayedColumns: string[] = ['students', 'school_origin', 'school_correcting', 'cross_corrector'];
   displayedColumns2: string[] = ['schools', 'students', 'correction', 'diff'];
-  dataSource = new MatTableDataSource<Student_Table>();
+
+  stdDataSource = new MatTableDataSource<Student_Table>();
+
   dataSource2 = new MatTableDataSource<School>();
+
+  dataSubs?: Subscription;
+
 
   @ViewChild(MatPaginator, { static: false })
   paginator!: MatPaginator;
@@ -37,6 +58,22 @@ export class AppComponent implements OnInit, AfterViewInit {
   )
   {}
 
+
+  insertDataCC(e:any, i:any)
+  {
+    this.studentTbl[i].school_correcting_corrector_id = e;
+  }
+
+  insertDataSC(e:any, i:any)
+  {
+    this.studentTbl[i].school_correcting_id = e;
+  }
+
+  getSchoolE(e:any)
+  {
+    return this.schOpt.filter(sc => sc.school._id !== e);
+  }
+
   ngOnInit()
   {
     this.getStudentTbl();
@@ -44,21 +81,106 @@ export class AppComponent implements OnInit, AfterViewInit {
     this.getSchoolList();
     this.getCorrectorList();
     this.getSchCorrLs();
+
+
+    this.stdFilOpt = this.stdControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this.stdFilter(value))
+    );
+
+    this.schFilOpt = this.schControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this.schFilter(value)),
+    );
+
+    this.corrFilOpt = this.corrControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this.corrFilter(value)),
+    );
+
   }
 
   ngAfterViewInit(): void {
-    this.dataSource.paginator = this.paginator;
-    this.dataSource2.paginator = this.paginator;
-}
+    // if(this.stdDataSource.data) this.stdDataSource.paginator = this.paginator;    
+  }
+
+  private stdFilter(value:string) 
+  {
+    if(!value)
+    {
+      this.reset();
+      return this.studentTbl;
+    }else{
+      const filterValue = value.toLowerCase();
+      return this.studentTbl.filter(std => std.student_id.first_name.toLocaleLowerCase().includes(filterValue) || std.student_id.last_name.toLocaleLowerCase().includes(filterValue) );  
+    }
+  }
+
+  private schFilter(value:string) 
+  {
+    if(!value)
+    {
+      this.reset();
+      return this.schoolLs;
+    }else{
+      return this.schoolLs.filter(sch => sch.school.short_name.toLocaleLowerCase().includes(value.toLowerCase()));  
+    }
+  }
+
+  private corrFilter(value:string) 
+  {
+    if(!value)
+    {
+      this.reset();
+      return this.correctorLs;
+    }else{
+      return this.correctorLs.filter(corr => corr.school_correcting_corrector_id.first_name.toLocaleLowerCase().includes(value.toLowerCase()));  
+    }
+  }
+
+  filterStdDatasource(stdId:string)
+  {
+    this.stdDataSource = new MatTableDataSource<Student_Table>(this.studentTbl);
+    this.stdDataSource.data = this.stdDataSource.data.filter(std => std.student_id._id.toLocaleLowerCase().includes(stdId));
+  }
+
+  filterSchDatasource($event:any, schId:string)
+  {    
+    if($event.isUserInput)
+    {
+      this.stdDataSource = new MatTableDataSource<any>(this.studentTbl);
+      this.stdDataSource.data = this.stdDataSource.data.filter(sch => sch?.school_origin_id?._id.toLocaleLowerCase().includes(schId));  
+    }
+  }
+
+  filterCorrDatasource($event:any, corrId:string)
+  {    
+    if($event.isUserInput)
+    {
+      this.stdDataSource = new MatTableDataSource<any>(this.studentTbl);
+      this.stdDataSource.data = this.stdDataSource.data.filter(sch => sch?.school_origin_id?._id.toLocaleLowerCase().includes(corrId));  
+    }
+  }
 
   getStudentTbl()
   {
-    this.apiSrvc.getStudentTbl().subscribe(data =>
+    this.dataSubs = this.apiSrvc.getStudentTbl().subscribe(data =>
     {
+      this.stdDataSource = new MatTableDataSource<Student_Table>(data);
+      this.stdDataSource.paginator = this.paginator;
       this.studentTbl = data;
-      this.dataSource.data = data;
-      console.log(data);
+      this.stdOpt = data;
     });
+  }
+
+  getDataWithNoCor()
+  {
+    return this.studentTbl.filter(dt => dt.school_correcting_corrector_id._id == null && dt.school_correcting_id._id == null).length
+  }
+
+  getScCor(e:any)
+  {
+    return this.schCorrLs.filter(scc => scc.school._id == e.school_correcting_id._id)[0].cross_correctors;
   }
 
   getSchoolTbl()
@@ -68,7 +190,8 @@ export class AppComponent implements OnInit, AfterViewInit {
       this.schoolTbl = data;
       this.dataSource2.data = data;
       this.totalSchool = data.length;
-      console.log(data);
+      console.log(data)
+
     });
   }
 
@@ -77,17 +200,14 @@ export class AppComponent implements OnInit, AfterViewInit {
     this.apiSrvc.getScList().subscribe(data=>
     {
       this.schoolLs = data;
-      console.log(data.length);
+      this.schOpt = data;
     });
-
-   
   }
 
   getCorrectorList()
   {
     this.apiSrvc.getCorList().subscribe(data=>
     {
-      console.log(data);
       this.correctorLs = data;
     });
   }
@@ -97,8 +217,19 @@ export class AppComponent implements OnInit, AfterViewInit {
     this.apiSrvc.getSchCorList().subscribe(data => 
     {
       this.schCorrLs = data;
-      console.log(data);
+
     })
+  }
+
+  reset()
+  {
+    this.schControl.reset('',{emitEvent:false});
+    this.stdControl.reset('',{emitEvent:false});
+    this.corrControl.reset('',{emitEvent:false});
+    this.scControl.reset('',{emitEvent:false});
+    this.ccControl.reset('',{emitEvent:false});
+    this.stdDataSource.data = this.studentTbl;
+    this.stdDataSource.paginator = this.paginator;
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,6 +14,11 @@ import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
 import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import { FlexLayoutModule, FlexModule, GridModule } from '@angular/flex-layout';
+import { MatSelectModule } from '@angular/material/select';
 
 
 @NgModule({
@@ -29,7 +35,16 @@ import { MatInputModule } from '@angular/material/input';
     MatTableModule,
     MatPaginatorModule,
     MatSortModule,
-    MatInputModule
+    MatInputModule,
+    MatIconModule,
+    MatAutocompleteModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FlexLayoutModule,
+    FlexModule,
+    GridModule,
+    MatButtonModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
